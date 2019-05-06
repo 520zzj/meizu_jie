2353d7fa71b4985bfa5ad673576a1b2bc094c690
520zzj <44475605+520zzj@users.noreply.github.com>
2019-05-07 02:49:09 +0800

    商品列表和指示器的相关图片

--- server/routes/productList_router.js
+++ server/routes/productList_router.js
@@ -15,24 +15,40 @@ router.get("/",(req,res)=>{
     ps=parseInt(ps)
     var progress=0;
     var data={}
-    var sql=`select pid,pname,pdesc,pprice,psrc,psign from product_list limit ?,?`
+    var sql=`select pid,pname,pdesc,pprice,psign from product_list limit ?,?`
+  //商品信息
     pool.query(sql,[offset,ps],(err,result)=>{
         if(err) throw err;
         progress+=50
         data.prolis=result
-        if(progress==100){
+        if(progress==150){
             res.send(data)
         }
     })
 
+
     var sql2=`select iid,cname,csrc,psrc,prolisId from icon_color_src`
+   //指示器保存的信息
     pool.query(sql2,[],(err,result)=>{
         if(err) throw err;
         progress+=50
         data.color=result
-        if(progress==100){
+        if(progress==150){
             res.send(data)
         }
     })
+//获取商品个数，来确定共有多少页数
+    var sql3=`select count(pid) as mount from product_list`
+    pool.query(sql3,[],(err,result)=>{
+        if(err) throw err;
+        //计算总页数
+        data.pages=Math.ceil(result[0].mount/ps)
+        progress+=50
+        if(progress==150)
+        res.send(data)
+    })
 })
+
+ 
+
 module.exports=router
