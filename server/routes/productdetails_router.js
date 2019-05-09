78fb7549105025b79eef6d3e0c54db3baa4654c0
520zzj <44475605+520zzj@users.noreply.github.com>
2019-05-10 02:14:21 +0800

    Update productdetails_router.js
    从花呗分期数据从数据库提取

--- server/routes/productdetails_router.js
+++ server/routes/productdetails_router.js
@@ -2,10 +2,13 @@ const express=require("express")
 const router=express.Router()
 const pool=require("../pool")
 
-router.get("/huabei",(req,res)=>{
-    //临时构建一个数组
-    var arr=[[{periods:"¥466.00×3期",rate:"免费手续"},{periods:"¥233.00×6期",rate:"免费手续"},{periods:"¥125.23×12期",rate:"免费手续"}],[{periods:"¥532.66×3期",rate:"免费手续"},{periods:"¥266.33×6期",rate:"免费手续"},{periods:"¥143.14×12期",rate:"含手续费 ￥9.98/期"}]]
-    res.send(arr)
+router.get("/",(req,res)=>{
+   
+   var sql=`select id,periods,rate,sid from product_details`
+   pool.query(sql,[],(err,result)=>{
+        if(err) throw err;
+        res.send(result)
+   })
 })
 
 module.exports=router;
