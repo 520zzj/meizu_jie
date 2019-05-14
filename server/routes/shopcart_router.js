e29959ac02a2d6bba511f755f68bc7c814133ca4
520zzj <44475605+520zzj@users.noreply.github.com>
2019-05-14 18:05:50 +0800

    Create shopcart_router.js
    查询和删除功能

---
+++ server/routes/shopcart_router.js
@@ -0,0 +1,23 @@
+const express=require("express")
+const pool=require("../pool")
+const router=express.Router()
+
+router.get("/",(req,res)=>{
+    var sql=`select id,img_src,pname,props,unitprice,amount,subtotal from shopcart`
+    pool.query(sql,[],(err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+router.get("/delete",(req,res)=>{
+    var id=req.query.id;
+    var sql=`delete from shopcart where id=?`
+    pool.query(sql,[id],(err,result)=>{
+        if(err) throw err;
+        if(result.affectedRows!=0)
+        res.send({code:1,msg:"删除成功"})
+        else
+        res.send({code:-1,msg:"删除失败"})
+    })
+})
+module.exports=router
