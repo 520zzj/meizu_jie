8860418fb3ed02a6015dfffe1a5e2e57ea545a24
520zzj <44475605+520zzj@users.noreply.github.com>
2019-06-20 01:45:35 +0800

    更新购物车页面功能
    完善删除单个商品的功能，初步完成全选和单选的功能。

--- public/js/shopcart.js
+++ public/js/shopcart.js
@@ -1,53 +1,53 @@
 (function(){
+    function loadhtml(res){
+        var html=""
+        for(var p of res){
+            html+=`<div class="one_pro">
+            <div class="add">
+                <span class="add_sign">加价购</span>
+                <span class="add_amount">另加15元起，即可换购超值商品</span>
+                <a href="" class="add_now">立即加购 &gt;</a>
+            </div>
+            <div class="pro_lists">
+                <ul class="list_item">
+                    <li class="col-choose">
+                        <div class="checkbox" data-toggle="sigleBox"></div>
+                        <a href="" class="img_link">
+                                <img class="little_img"
+                                src=${p.img_src} alt="">
+                        </a>
+                        <div class="desc">
+                            <h3 class="name">${p.pname}</h3>
+                            <p class="config">${p.props}</p>
+                        </div>
+                    </li>
+                    <li class="col-price">
+                        <em>￥</em><span>${p.unitprice}</span>
+                    </li>
+                    <li class="col-mount">
+                        <div class="mount_box">
+                            <span class="disable">-</span><input type="text" value=${p.amount}><span>+</span>
+                        </div>
+                    </li>
+                    <li class="col-subtotal"> 
+                        <em>￥</em><span>${p.subtotal}</span>
+                    </li>
+                    <li class="col-edit">
+                        <div data-id=${p.id}>--</div>
+                    </li>
+                </ul>
+            </div>
+        </div>`
+        }
+        var proBox=document.querySelector(".pro_box")
+        proBox.innerHTML=html;
+    };
     ajax({
         method:"get",
         url:"http://127.0.0.1:9000/shopcart",
         dataType:"json"
     }).then(res=>{
         console.log(res);
-        function loadhtml(res){
-            var html=""
-            for(var p of res){
-                html+=`<div class="one_pro">
-                <div class="add">
-                    <span class="add_sign">加价购</span>
-                    <span class="add_amount">另加15元起，即可换购超值商品</span>
-                    <a href="" class="add_now">立即加购 &gt;</a>
-                </div>
-                <div class="pro_lists">
-                    <ul class="list_item">
-                        <li class="col-choose">
-                            <div class="checkbox"></div>
-                            <a href="" class="img_link">
-                                    <img class="little_img"
-                                    src=${p.img_src} alt="">
-                            </a>
-                            <div class="desc">
-                                <h3 class="name">${p.pname}</h3>
-                                <p class="config">${p.props}</p>
-                            </div>
-                        </li>
-                        <li class="col-price">
-                            <em>￥</em><span>${p.unitprice}</span>
-                        </li>
-                        <li class="col-mount">
-                            <div class="mount_box">
-                                <span class="disable">-</span><input type="text" value=${p.amount}><span>+</span>
-                            </div>
-                        </li>
-                        <li class="col-subtotal"> 
-                            <em>￥</em><span>${p.subtotal}</span>
-                        </li>
-                        <li class="col-edit">
-                            <div data-id=${p.id}>--</div>
-                        </li>
-                    </ul>
-                </div>
-            </div>`
-            }
-            var proBox=document.querySelector(".pro_box")
-            proBox.innerHTML=html;
-        }
         loadhtml(res);
 
         //页面滚动到一定距离，结算栏浮动到底部，侧边栏显示和隐藏
@@ -123,16 +123,20 @@
                 div.style.display="none"
             })
         })();
-        //编辑/完成,删除商品
+         //给“编辑”和“完成”添加点击事件，发生切换
         (function(){
+        //找到“编辑，完成”处的元素，并添加点击事件
             var edit=document.querySelector("[data-toggle=edit]")
-            var lis=document.querySelectorAll(".list_item .col-edit")
             edit.addEventListener("click",function(){
-                if(edit.innerHTML=="编辑"){
-                    edit.innerHTML="完成"
+        //找出要发生改变的元素集合
+                var lis=document.querySelectorAll(".list_item .col-edit")
+                //由编辑切换成完成时，所有的商品操作为可删除状态，符号变x
+                if(this.innerHTML=="编辑"){
+                    this.innerHTML="完成"
                     for(var  li of lis){
                         li.children[0].innerHTML="x"
                     }
+                //由完成切换成编辑时，所有的商品操作为不可删除状态，符号变--
                 }else if(this.innerHTML=="完成"){
                     this.innerHTML="编辑"
                     for(var li of lis){
@@ -140,44 +144,134 @@
                     }
                 }
             })
-            //删除单个商品
-            var delTip=document.querySelector("[data-del=tip]")
-             //确认框的操作 
-             var btnDel=document.querySelector("[data-btn=del]")
-             var btnCancel=document.querySelector("[data-btn=cancel]")
-            for(let li of lis){
-                li.children[0].addEventListener("click",function(){
-                    //当编辑列内容为x的时候才执行点击事件
-                    if(li.children[0].innerHTML=="x"){
-                        //先弹出确认删除确认框
-                        delTip.style.display="block"
-                        //点击删除按钮
-                        btnDel.addEventListener("click",function(){
-                        //获取该商品的id,发送ajax请求,删除该商品，并刷新页面
-                          var id=li.children[0].dataset.id    
-                          ajax({
-                              method:"get",
-                              url:"http://127.0.0.1:9000/shopcart/delete",
-                              dataType:"json",
-                              data:"id="+id
-                          }).then(res=>{
-                             //删除商品后，刷新商品
-                              ajax({
-                                  method:"get",
-                                  url:"http://127.0.0.1:9000/shopcart",
-                                  dataType:"json"
-                              }).then(result=>{
-                                loadhtml(result);
-                                //确认框隐藏
-                                delTip.style.display="none"
-                              })
-                          })
+        })();
+        //编辑/完成,删除商品
+        (function(){
+            var lis=document.querySelectorAll(".list_item .col-edit")
+        //如何在页面刷新后，给商品从新绑定点击事件？
+            console.log(lis)
+        //删除单个商品
+        //获取删除确认框元素
+        var delTip=document.querySelector("[data-del=tip]")
+            //确认框的操作 
+            //获取确认框删除和取消按钮元素
+            var btnDel=document.querySelector("[data-btn=del]")
+            var btnCancel=document.querySelector("[data-btn=cancel]")
+            //给每个商品绑定点击事件
+        for(let li of lis){
+            li.children[0].addEventListener("click",function(){
+                //当编辑列内容为x的时候才执行点击事件
+                if(li.children[0].innerHTML=="x"){
+                    //先弹出确认删除确认框
+                    delTip.style.display="block"
+                    //点击删除按钮
+                    btnDel.addEventListener("click",function(){
+                    //获取该商品的id,发送ajax请求,删除该商品，并刷新页面
+                        var id=li.children[0].dataset.id    
+                        ajax({
+                            method:"get",
+                            url:"http://127.0.0.1:9000/shopcart/delete",
+                            dataType:"json",
+                            data:"id="+id
+                        }).then(res=>{
+                            //删除商品后，刷新商品
+                            ajax({
+                                method:"get",
+                                url:"http://127.0.0.1:9000/shopcart",
+                                dataType:"json"
+                            }).then(result=>{
+                            //确认框隐藏
+                            delTip.style.display="none"
+                            //刷新商品
+                            loadhtml(result);
+                            //刷新商品列表后，依然是可删除状态，可以继续删除其他商品
+                            //获取dom树重绘后的x或--对应的元素，否则无法获取更新后的x或--
+                            lis=document.querySelectorAll(".list_item .col-edit")
+                            //让删除商品后依然保持x，可继续删除其余商品
+                            for(var li of lis){
+                            li.children[0].innerHTML="x"
+                            }
+                            // function taskx(lis){
+                            //     for(var  li of lis){
+                            //         li.children[0].innerHTML="x"
+                            //         console.log(1)
+                            //     }
+                            // }
+                            // var p=new Promise(function(resolve,reject){
+                            //     resolve();
+                            // })
+                            // p.then(loadhtml(result)).then(taskx(lis))
+                            })
+
                         })
+                    })
+                }
+            })
+            
+        }
+
+
+        })();
+        //选择商品和没有选中切换
+        (function(){
+            //找出所有选择框
+            var boxs=document.querySelectorAll(".cart .checkbox")
+            //找出所有全选框
+            var selAll=document.querySelectorAll("[data-toggle=selAll]")
+            // 找出所有的非全选框
+            var sigleBoxs=document.querySelectorAll("[data-toggle=sigleBox]")
+            //把类数组对象转换成数组
+            sigleBoxs=Array.from(sigleBoxs)
+            for(let box of boxs){
+                box.addEventListener("click",function(){
+                    // if(box.classList.contains("checked")){
+                    //     box.classList.remove("checked")
+                    // }else{
+                    //     box.classList.add("checked")
+                    // }
+                    //选上时，
+                    //当前点击按钮如果是全选按钮，所有的按钮都选上，添加checked类
+                    //否则仅仅当前按钮选上，添加checked类
+                    //取消时，
+                    //当前点击按钮如果是全选按钮，所有的按钮都取消，去掉checked类
+                    //否则去掉当前按钮和全选按钮上的checked类
+                    //当非全选按钮都选上时，全选按钮也选上
+                    if(!this.classList.contains("checked")){
+                        if(this.dataset.toggle=="selAll"){
+                            for(var i=0;i<boxs.length;i++){
+                                boxs[i].classList.add("checked")
+                            }
+                        }else{
+                            this.classList.add("checked")
+                        }
+                    }else{
+                        if(this.dataset.toggle=="selAll"){
+                            for(var i=0;i<boxs.length;i++){
+                                boxs[i].classList.remove("checked")
+                            }
+                        }else{
+                            this.classList.remove("checked")
+                            for(var j=0;j<selAll.length;j++){
+                                selAll[j].classList.remove("checked")
+                            }
+                        }
                     }
+                    //非全选框都选上，即是都含有checked类时，全选框也选上
+                    var flag=sigleBoxs.every(function(elem,i,arr){
+                        return elem.classList.contains("checked")
+                    })
+                    if(flag){
+                        for(var sa of selAll){
+                            sa.classList.add("checked")
+                        }
+                    }
+                  
                 })
             }
            
-        
         })();
-    })
+
+    });
+
 })();
+ 
