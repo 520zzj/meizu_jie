e4f0763a0162d64607e0a6a3c353edcdddb50bec
520zzj <44475605+520zzj@users.noreply.github.com>
2019-06-22 19:00:46 +0800

    Update shopcart_router.js
    新增批量删除的接口

--- server/routes/shopcart_router.js
+++ server/routes/shopcart_router.js
@@ -20,4 +20,15 @@ router.get("/delete",(req,res)=>{
         res.send({code:-1,msg:"删除失败"})
     })
 })
+router.get("/delMore",(req,res)=>{
+    var id=req.query.ids
+    var sql="delete from shopcart where id in ("+id+")"
+    pool.query(sql,[],(err,result)=>{
+        if(err) throw err;
+        if(result.affectedRows!=0)
+        res.send({code:1,msg:"删除成功"})
+        else
+        res.send({code:-1,msg:"删除失败"})
+    })
+})
 module.exports=router
