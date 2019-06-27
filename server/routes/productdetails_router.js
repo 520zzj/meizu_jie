628efc5a165fa836d2c7024480b50da29feeddaf
520zzj <44475605+520zzj@users.noreply.github.com>
2019-06-28 01:24:49 +0800

    Update productdetails_router.js
    修改花呗的表名

--- server/routes/productdetails_router.js
+++ server/routes/productdetails_router.js
@@ -4,7 +4,7 @@ const pool=require("../pool")
 
 router.get("/",(req,res)=>{
    
-   var sql=`select id,periods,rate,sid from product_details`
+   var sql=`select id,periods,rate,sid from pro_det_huabei`
    pool.query(sql,[],(err,result)=>{
         if(err) throw err;
         res.send(result)
